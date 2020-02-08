7745c22b933d5ca31affbc6db422081756b515a4
Martin K <mrtn.karlsson@gmail.com>
2020-02-08 14:25:24 +0100

    small stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.njCompolet1 = new OMRON.Compolet.CIP.NJCompolet(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -47,6 +46,7 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,9 +64,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(17, 174);
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 59);
+            this.button1.Size = new System.Drawing.Size(94, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,9 +75,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(17, 239);
+            this.button2.Location = new System.Drawing.Point(11, 46);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(188, 59);
+            this.button2.Size = new System.Drawing.Size(94, 31);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -84,117 +86,130 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(17, 304);
+            this.button3.Location = new System.Drawing.Point(11, 81);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(188, 59);
+            this.button3.Size = new System.Drawing.Size(94, 31);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 25);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(294, 191);
+            this.label1.Location = new System.Drawing.Point(345, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 31);
+            this.label1.Size = new System.Drawing.Size(41, 19);
             this.label1.TabIndex = 5;
-            this.label1.Text = "btn_label1";
+            this.label1.Text = "Time";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(294, 256);
+            this.label3.Location = new System.Drawing.Point(147, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(139, 31);
+            this.label3.Size = new System.Drawing.Size(0, 17);
             this.label3.TabIndex = 6;
-            this.label3.Text = "btn_label2";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(294, 321);
+            this.label4.Location = new System.Drawing.Point(147, 167);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 31);
+            this.label4.Size = new System.Drawing.Size(0, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "btn_label3";
             // 
             // chart1
             // 
-            chartArea2.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
-            chartArea2.AxisX.Title = "Time";
-            chartArea2.AxisY.Title = "Temp";
-            chartArea2.Name = "TempGraph";
-            this.chart1.ChartAreas.Add(chartArea2);
-            this.chart1.Location = new System.Drawing.Point(17, 477);
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.AxisX.Title = "Time";
+            chartArea1.AxisY.Title = "Temp";
+            chartArea1.Name = "TempGraph";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(11, 304);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "TempGraph";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.IsXValueIndexed = true;
-            series2.Legend = "Legend1";
-            series2.Name = "Temperature";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(1111, 467);
+            series1.ChartArea = "TempGraph";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperature";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(782, 243);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
-            title2.DockedToChartArea = "TempGraph";
-            title2.Name = "Temp Thing";
-            this.chart1.Titles.Add(title2);
+            title1.DockedToChartArea = "TempGraph";
+            title1.Name = "Temp Thing";
+            this.chart1.Titles.Add(title1);
             // 
             // btn_get_names
             // 
-            this.btn_get_names.Location = new System.Drawing.Point(809, 49);
+            this.btn_get_names.AutoSize = true;
+            this.btn_get_names.Location = new System.Drawing.Point(589, 3);
+            this.btn_get_names.Margin = new System.Windows.Forms.Padding(2);
             this.btn_get_names.Name = "btn_get_names";
-            this.btn_get_names.Size = new System.Drawing.Size(121, 44);
+            this.btn_get_names.Size = new System.Drawing.Size(204, 23);
             this.btn_get_names.TabIndex = 9;
-            this.btn_get_names.Text = "Get Val";
+            this.btn_get_names.Text = "Update Tags";
             this.btn_get_names.UseVisualStyleBackColor = true;
             this.btn_get_names.Click += new System.EventHandler(this.btn_getvars);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(809, 409);
+            this.textBox1.Location = new System.Drawing.Point(668, 267);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 31);
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
             this.textBox1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(809, 99);
+            this.listBox1.Location = new System.Drawing.Point(589, 51);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(246, 304);
+            this.listBox1.Size = new System.Drawing.Size(204, 212);
             this.listBox1.TabIndex = 11;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(936, 49);
+            this.button4.Location = new System.Drawing.Point(588, 267);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 44);
+            this.button4.Size = new System.Drawing.Size(76, 20);
             this.button4.TabIndex = 12;
             this.button4.Text = "Set Val";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(654, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Available Tags";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1140, 956);
+            this.ClientSize = new System.Drawing.Size(804, 558);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
@@ -203,12 +218,11 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
@@ -222,7 +236,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -232,6 +245,7 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private NJCompolet myNJ2 = new NJCompolet();
+        private readonly NJCompolet myNJ2 = new NJCompolet();
 
         public Form1()
         {
@@ -23,6 +23,7 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            timer1.Start();
             try
             {
                 myNJ2.UseRoutePath = false;
@@ -30,27 +31,49 @@ namespace WindowsFormsApp1
                 myNJ2.LocalPort = 2;
                 myNJ2.Active = true;
 
-                label2.Text = myNJ2.UnitName;
-            
-                
+                Text = myNJ2.UnitName;
+                Get_Tags();    
             }
             catch (Exception ex)
             {
-                label2.Text = ex.Message;
+                Text = ex.Message;
             }
-
-
         }
 
-        private void Get_Temp(object sender, EventArgs e)
+        private void Get_Temp()
         {
-            int nj2Temp = (int)myNJ2.ReadVariable("temperatur_verklig");
-            chart1.Series["Temperature"].Points.AddXY(DateTime.Now, nj2Temp);
+            try
+            {
+                int nj2Temp = (int)myNJ2.ReadVariable("temperatur_verklig");
+                chart1.Series["Temperature"].Points.AddXY(DateTime.Now.ToString("T"), nj2Temp);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Get_Tags()
+        {
+            try
+            {
+                var var_names = myNJ2.VariableNames;
+
+                listBox1.Items.Clear();
+
+                foreach (var item in var_names)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            catch(Exception e)
+            {
+                label5.Text = e.Message;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            button1.Text = "Hej";
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -64,25 +87,36 @@ namespace WindowsFormsApp1
 
         private void btn_getvars(object sender, EventArgs e)
         {
-            var var_names = myNJ2.VariableNames;
-
-            listBox1.Items.Clear();
-
-            foreach (var item in var_names)
-            {
-                listBox1.Items.Add(item);
-            }
-
+            Get_Tags();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            textBox1.Text = (string)myNJ2.ReadVariable(listBox1.SelectedItem.ToString());
+            try
+            {
+                textBox1.Text = (string)myNJ2.ReadVariable(listBox1.SelectedItem.ToString());
+            }
+            catch (Exception)
+            {
+                textBox1.Text = "Failed to fetch selected";
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            myNJ2.WriteVariable((string)listBox1.SelectedItem, textBox1.Text);
+            try
+            {
+                myNJ2.WriteVariable((string)listBox1.SelectedItem, textBox1.Text);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("T");
+            Get_Temp();
         }
     }
 }
